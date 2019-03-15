44875756319295ee2a52b4d52a51f5265ac155bc
124 <123@macbook-pro-karol-2.home>
2019-03-15 23:56:11 +0100

    fastmail

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -22,7 +22,8 @@ public class EmailScheduler {
 
     private static final String SUBJECT = "Tasks: Once a day email";
 
-    @Scheduled(cron="0 0 10 * * *")
+    //@Scheduled(cron="0 0 10 * * *")
+    @Scheduled(fixedDelay = 5000)
     public void sendInformationEmail() {
         long size = taskRepository.count();
         simpleEmailService.send(new Mail(
