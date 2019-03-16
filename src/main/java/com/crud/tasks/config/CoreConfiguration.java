3d7c090a0cbc491fffda890f18ad8dd0711541d8
124 <123@macbook-pro-karol-2.home>
2019-03-16 09:49:52 +0100

    core

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -34,7 +34,8 @@ public class CoreConfiguration implements WebMvcConfigurer {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
+//                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
