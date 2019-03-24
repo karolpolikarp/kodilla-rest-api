cff90c103a5cc55f51938b5cfc1d2958cbeacd7b
124 <123@MBP-Karol-2.chello.pl>
2019-03-24 20:44:42 +0100

    reset

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -20,17 +20,17 @@ public class EmailScheduler {
     @Autowired
     private AdminConfig adminConfig;
 
-    private static final String SUBJECT = "Wykonałeś przelew - proszę o kontakt +86 8876234232";
+    private static final String SUBJECT = "Tasks: Once a day email";
 
-//    @Scheduled(cron = "0 0 10 * * *")
-    @Scheduled(fixedDelay = 5000)
+    @Scheduled(cron = "0 0 10 * * *")
+//    @Scheduled(fixedDelay = 5000)
     public void sendInformationEmail() {
         long size = taskRepository.count();
         String taskOrTasks = (size != 1) ? "tasks" : "task";
         simpleEmailService.send(new Mail(
                 adminConfig.getAdminMail(),
                 SUBJECT,
-                "65.234.255.12, ul. Obrzeżna - Warszawa (52.173212 x 20.999425)",
+                "Currently in database you've got: " + size + "" + taskOrTasks,
                 null
         ));
     }
