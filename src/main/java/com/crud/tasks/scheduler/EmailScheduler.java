be09e6277d6ebab6791032b860159830d9850877
124 <123@MBP-Karol-2.chello.pl>
2019-03-24 21:36:53 +0100

    gg

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -20,17 +20,17 @@ public class EmailScheduler {
     @Autowired
     private AdminConfig adminConfig;
 
-    private static final String SUBJECT = "420 420 420 420";
+    private static final String SUBJECT = "PK 1 : 0 MK FIFA";
 
 //    @Scheduled(cron = "0 0 10 * * *")
-//    @Scheduled(fixedDelay = 1000)
+    @Scheduled(fixedDelay = 1000)
     public void sendInformationEmail() {
         long size = taskRepository.count();
         String taskOrTasks = (size != 1) ? "tasks" : "task";
         simpleEmailService.send(new Mail(
                 adminConfig.getAdminMail(),
                 SUBJECT,
-                "jaranko",
+                "NAUCZ SIĘ GRAĆ CIOTO",
                 null
         ));
     }
