d477b5ef848618db94694cad4e34bdf1b305cf75
karolpolikarp <karolpwilczynski@gmail.com>
2019-05-28 00:08:22 +0200

    heorkufaoult

--- src/main/java/com/crud/tasks/trello/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/trello/service/MailCreatorService.java
@@ -1,13 +1,23 @@
 package com.crud.tasks.trello.service;
 
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.time.LocalDate;
+
 @Service
 public class MailCreatorService {
+
+    @Autowired
+    private AdminConfig adminConfig;
+    @Autowired
+    private CompanyConfig companyConfig;
 
     @Autowired
     @Qualifier("templateEngine")
@@ -16,6 +26,11 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("company_info", companyConfig.getCompanyInfo());
+        context.setVariable("goodbye_message", "Have a nice " + StringUtils.capitalize(LocalDate.now().getDayOfWeek().name().toLowerCase()));
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
