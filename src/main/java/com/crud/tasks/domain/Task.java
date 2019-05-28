d94425853441464b9e0164bf1344dc0d77666c4e
karolpolikarp <karolpwilczynski@gmail.com>
2019-05-28 18:49:06 +0200

    Task entity

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Entity(name = "tasks")
 public class Task {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private Long id;
 
     @Column(name = "name")
--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -2,14 +2,12 @@ package com.crud.tasks.domain;
 
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
 public class TaskDto {
     private Long id;
     private String title;
